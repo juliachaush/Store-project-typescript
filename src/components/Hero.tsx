03a40ec05a65e65ccb117cdf979ed28e8d77b3f2
juliachaush <juliachaush@gmail.com>
2024-10-27 21:20:20 +0100

    add hero screen

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,26 @@
+import { Link } from "react-router-dom";
+import { Button } from "./ui/button";
+import HeroCarousel from "./HeroCarousel";
+
 function Hero() {
-  return <div>Hero</div>;
+  return (
+    <section className="grid grid-cols-1 lg:grid-cols-2 gap-24 items-center">
+      <div>
+        <h1 className="max-w-2xl font-bold text-4xl tracking-tight sm:text-6xl">
+          We are changing the way people shop
+        </h1>
+        <p className="mt-8 max-w-xl text-lg leading-8">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. In explicabo
+          ipsa beatae alias. Vel, quibusdam aliquam id voluptatum quod possimus
+          minima rem vero ratione aliquid tempora voluptate, enim recusandae
+          architecto?
+        </p>
+        <Button asChild size="lg" className="mt-10">
+          <Link to="/products">Our Products</Link>
+        </Button>
+      </div>
+      <HeroCarousel />
+    </section>
+  );
 }
 export default Hero;
