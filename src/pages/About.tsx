4c992ad473a64a3349dab04613495ef7c7606db5
juliachaush <juliachaush@gmail.com>
2024-10-27 10:37:32 +0100

    add about block

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,4 +1,19 @@
 function About() {
-  return <h1 className="text-4xl">About Page</h1>;
+  return (
+    <section>
+      <h1 className="flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center text-4xl font-bold leading-none tracking-wide sm:text-6xl">
+        We love {""}
+        <span className="bg-primary py-2 px-4 rounded-lg tracking-widest text-white">
+          comfy
+        </span>
+      </h1>
+      <p className="mt-6 text-lg tracking-wide leading-8 max-w-2xl mx-auto">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet iure
+        dolores natus quisquam corrupti fugiat molestiae dolorem perspiciatis
+        optio inventore illum, iste ducimus quia doloremque est impedit harum
+        sed quo.
+      </p>
+    </section>
+  );
 }
 export default About;
