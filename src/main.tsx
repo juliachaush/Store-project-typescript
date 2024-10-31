97a965f8336001c0aa20b3b3eea54ce4e420e9ae
juliachaush <juliachaush@gmail.com>
2024-10-31 10:35:13 +0100

    add cart slice

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
-import { store } from './store';
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { store } from "./store";
+import { Provider } from "react-redux";
+import { Toaster } from "@/components/ui/toaster";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
+    <Toaster />
     <App />
   </Provider>
 );
