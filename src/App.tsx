38c9ef3b0ee318328a2a6c50d8633288ae32311a
juliachaush <juliachaush@gmail.com>
2024-10-31 21:44:50 +0100

    add register form and user slice

--- src/App.tsx
+++ src/App.tsx
@@ -22,6 +22,8 @@ import { loader as landingLoader } from "./pages/Landing";
 import { loader as productsLoader } from "./pages/Products";
 import { loader as singleProductLoader } from "./pages/SingleProduct";
 
+import { action as registerUser } from "./pages/Register";
+
 const router = createBrowserRouter([
   {
     path: "/",
@@ -77,6 +79,7 @@ const router = createBrowserRouter([
     path: "/register",
     element: <Register />,
     errorElement: <Error />,
+    action: registerUser,
   },
 ]);
 
