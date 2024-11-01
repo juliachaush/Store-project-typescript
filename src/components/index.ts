5394efaa4aa7dbb893b2544efd3f80cf5d9cf4ed
juliachaush <juliachaush@gmail.com>
2024-11-01 10:43:25 +0100

    add chackout form

--- src/components/index.ts
+++ src/components/index.ts
@@ -27,3 +27,4 @@ export { default as CartItemsList } from "./CartItemsList";
 export { default as CartTotals } from "./CartTotals";
 export { default as CartItemColumns } from "./CartItemColumns";
 export { default as SubmitBtn } from "./SubmitBtn";
+export { default as CheckoutForm } from "./CheckoutForm";
